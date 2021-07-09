ee5db5ab61018efb6292eeae7592b6e04488646d
Leonardo de Almeida <leodealmeida9@gmail.com>
2021-07-09 17:41:01 -0300

    chore: configure vue logger

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueLogger from 'vuejs-logger'
+
 Vue.config.productionTip = false
+
+const options = {
+  isEnabled: true,
+  logLevel : 'debug',
+  stringifyArguments : false,
+  showLogLevel : true,
+  showMethodName : false,
+  separator: '|',
+  showConsoleColors: true
+};
+
+Vue.use(VueLogger, options);
 
 new Vue({
   render: h => h(App),
