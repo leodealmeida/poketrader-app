f8f31015b13bea70d59bf0b550562941c5d35781
Leonardo de Almeida <leodealmeida9@gmail.com>
2021-07-11 16:31:12 -0300

    feat(trade): disable trade button if no pokemon is selected

--- src/components/trade/TradeOperations.vue
+++ src/components/trade/TradeOperations.vue
@@ -1,5 +1,5 @@
 <template>
-  <button v-on:click="submitTrade">trade</button>
+  <button v-on:click="submitTrade" :disabled="hasPokemon">trade</button>
 </template>
 
 <script>
@@ -8,6 +8,11 @@ import poketraderApiService from '../../services/poketraderApi'
 export default {
   name: 'TradeOperations',
   props: ['sideA', 'sideB'],
+  computed: {
+    hasPokemon() {
+      return !(this.sideA.length > 0 && this.sideB.length > 0)
+    }
+  },
   methods: {
     submitTrade() {
       const command = this.buildCommand();
